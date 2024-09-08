3eb6d3ab7c24f81fda5849cafa7562924bd8d449
Arpan <arpankumar2603@gmail.com>
2024-09-08 12:59:53 +0530

    JS added

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,21 @@
 <div class="footer">
-      <div class="row pglistfooter">
-        <div class="col-md pglist">
-          <a href="./property_list.php?city=Delhi">PG in Delhi</a>
-        </div>
-        <div class="col-md pglist">
-          <a href="./property_list.php?city=Mumbai">PG in Mumbai</a>
-        </div>
-        <div class="col-md pglist">
-          <a href="./property_list.php?city=Bengaluru">PG in Bangalore</a>
-        </div>
-        <div class="col-md pglist">
-          <a href="./property_list.php?city=Hyderabad">PG in Hyderabad</a>
-        </div>
-      </div>
-      <div class="copyright">
-        <span class="copy">©️ 2024 Copyright PG Life</span>
-      </div>
+  <div class="row pglistfooter">
+    <div class="col-md pglist">
+      <a href="./property_list.php?city=Delhi">PG in Delhi</a>
     </div>
+    <div class="col-md pglist">
+      <a href="./property_list.php?city=Mumbai">PG in Mumbai</a>
+    </div>
+    <div class="col-md pglist">
+      <a href="./property_list.php?city=Bengaluru">PG in Bangalore</a>
+    </div>
+    <div class="col-md pglist">
+      <a href="./property_list.php?city=Hyderabad">PG in Hyderabad</a>
+    </div>
+  </div>
+  <div class="copyright">
+    <span class="copy">©️ 2024 Copyright PG Life</span>
+  </div>
+</div>
+
+<script src="./js/common.js"></script>
